54e77a9d4fe3c8589331603f433dffbcf10314f9
Balázs Várhegyi <ezcryse@hotmail.com>
2019-10-31 00:01:28 +0100

    Folder changes

---
+++ board-game-framework/Assets/Scripts/Games/Catan/Cards/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public GameObject knightCardPrefab;
+    public GameObject victoryPointCardPrefab;
+    public GameObject yearOfPlentyCardPrefab;
+    public GameObject roadBuildingCardPrefab;
+    public GameObject monopolyCardPrefab;
+    public GameObject cardInspector;
+    public List<ICard> cards;
+    void Start()
+    {
+        cards = new List<ICard>();
+
+        for (int i = 0; i < 15; i++)
+        {
+            cards.Add(Instantiate(knightCardPrefab, cardInspector.transform).GetComponent<ICard>());
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            cards.Add(Instantiate(victoryPointCardPrefab, cardInspector.transform).GetComponent<ICard>());
+        }
+
+        cards.Add(Instantiate(yearOfPlentyCardPrefab, cardInspector.transform).GetComponent<ICard>());
+        cards.Add(Instantiate(yearOfPlentyCardPrefab, cardInspector.transform).GetComponent<ICard>());
+
+        cards.Add(Instantiate(roadBuildingCardPrefab, cardInspector.transform).GetComponent<ICard>());
+        cards.Add(Instantiate(roadBuildingCardPrefab, cardInspector.transform).GetComponent<ICard>());
+
+        cards.Add(Instantiate(monopolyCardPrefab, cardInspector.transform).GetComponent<ICard>());
+        cards.Add(Instantiate(monopolyCardPrefab, cardInspector.transform).GetComponent<ICard>());
+
+        foreach (var c in cards)
+        {
+            c.GetGameObject().SetActive(false);
+        }
+    }
+
+    public ICard GetCard()
+    {
+        var random = Random.Range(0, cards.Count);
+        ICard card = cards[random];
+        cards.RemoveAt(random);
+        return card;
+    }
+}
---
+++ board-game-framework/Assets/Scripts/Games/Catan/Cards/KnightCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightCard : MonoBehaviour, ICard
+{
+    private bool _playable;
+    public void Play(GameController gameController)
+    {
+        gameController.KnightPlayedAction?.Invoke();
+    }
+
+    public void Playable()
+    {
+        _playable = true;
+    }
+
+    public bool GetPlayable()
+    {
+        return _playable;
+    }
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+}
