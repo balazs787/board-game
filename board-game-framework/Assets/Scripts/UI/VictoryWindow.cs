564005822caeb48b81e885062e5546b322b2da23
Balázs Várhegyi <ezcryse@hotmail.com>
2019-10-31 04:33:34 +0100

    Menu and back buttons

--- board-game-framework/Assets/Scripts/UI/VictoryWindow.cs
+++ board-game-framework/Assets/Scripts/UI/VictoryWindow.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryWindow : MonoBehaviour
 {
     public TextMeshProUGUI victoryText;
+    public GameObject backToMenuButton;
 
     public void Open(Player player)
     {
         gameObject.SetActive(true);
+        backToMenuButton.SetActive(true);
         victoryText.text = $"Congratulations!\n{player.playerName} won";
         victoryText.color = player.color;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
